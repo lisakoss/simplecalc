05eca32ea6fa22c02be3d9d9962ac9cf8113feb2
Lisa Koss <lisakoss@uw.edu>
2018-01-16 11:18:06 -0800

    Fix args count error and rearrange code

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,59 +10,54 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
-        
-        // basic operators (+, -, /, %, *)
-        if(args.count == 3) {
-            switch args[1] {
-            case "+":
-                return Int(args[0])! + Int(args[2])!
-            case "-":
-                return Int(args[0])! - Int(args[2])!
-            case "/":
-                return Int(args[0])! / Int(args[2])!
-            case "%":
-                return Int(args[0])! % Int(args[2])!
-            case "*":
-                return Int(args[0])! * Int(args[2])!
-            default:
-                return 0
-            }
-        } else { // complex operators (count, avg, fact)
+        // check for complex operators (count, avg, fact)
+        if(args.count > 1) {
             switch args[args.count - 1] {
             case "count":
-                if args.count == 1 {
-                    return 0
-                } else {
-                    var total = 0
-                    for _ in 0...(args.count - 2) {
-                        total += 1
-                    }
-                    return total
+                var total : Int = 0
+                for _ in 0...(args.count - 2) {
+                    total += 1
                 }
+                return total
+                
             case "avg":
-                if args.count == 1 {
-                    return 0
-                } else {
-                    var total = 0
-                    for i in 0...(args.count - 2) {
-                        total += Int(args[i])!
-                    }
-                    return total / (args.count - 1)
+                var total : Int = 0
+                for i in 0...(args.count - 2) {
+                    total += Int(args[i])!
                 }
+                return total / (args.count - 1)
+                
             case "fact":
-                if args.count == 1 {
-                    return 0
-                } else {
-                    var total = 1
-                    for i in 1...Int(args[0])! {
-                        total *= i
-                    }
-                    return total 
+                var total : Int = 1
+                for i in 1...Int(args[0])! {
+                    total *= i
                 }
-            default:
-                return 0
+                return total
+                
+            default: // else check for basic operators (+, -, /, %, *)
+                switch args[1] {
+                case "+":
+                    return Int(args[0])! + Int(args[2])!
+                    
+                case "-":
+                    return Int(args[0])! - Int(args[2])!
+                    
+                case "/":
+                    return Int(args[0])! / Int(args[2])!
+                    
+                case "%":
+                    return Int(args[0])! % Int(args[2])!
+                    
+                case "*":
+                    return Int(args[0])! * Int(args[2])!
+                    
+                default: // no match
+                    return 0
+                }
             }
         }
+        
+        return 0 // default with no match
     }
     
     public func calculate(_ arg: String) -> Int {
